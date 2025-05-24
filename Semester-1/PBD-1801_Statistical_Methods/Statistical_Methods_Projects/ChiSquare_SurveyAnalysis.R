c7b5b1ef5b771947265d2d0170e56b51a16f8ccc
itsmemauli <124795820+itsmemauliii@users.noreply.github.com>
2025-05-24 14:42:18 +0530

    Update ChiSquare_SurveyAnalysis.R

--- Semester-1/PBD-1801_Statistical_Methods/Statistical_Methods_Projects/ChiSquare_SurveyAnalysis.R
+++ Semester-1/PBD-1801_Statistical_Methods/Statistical_Methods_Projects/ChiSquare_SurveyAnalysis.R
@@ -1,15 +1,12 @@
 # Load data
 survey_data <- read.csv("survey_data.csv")
-
+survey_data
 # Create contingency table
 table_data <- table(survey_data$Gender, survey_data$ProductPreference)
-
+table_data
 # Chi-square test
 chisq.test(table_data)
-
 # Expected values
 chisq.test(table_data)$expected
-
 # Visualizing with mosaic plot
 mosaicplot(table_data, main = "Product Preference by Gender", col = TRUE)
-
